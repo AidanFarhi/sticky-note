a4a59fb1041a0b26aaa1492cb1d5f808adeddfff
Aidan Farhi <Papa@adias-mbp.home>
2020-06-15 12:51:31 -0400

    basic functions working. Needs a logout feature and some more debugging and styling

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ from flask import Flask, session, request, render_template, url_for, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from datetime import datetime
 
 
 app = Flask(__name__)
@@ -25,7 +26,7 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/", methods=["POST", "GET"])
 def index():
 
-    if method.get == "POST":
+    if request.method == "POST":
 
         username = request.form.get("username")
         password = request.form.get("password")
@@ -40,15 +41,18 @@ def index():
         if not user_check:
             return render_template('index.html', message="Username not found")
 
-        password_check = db.execute("SELECT * FROM users WHERE password = :password", "password": password)  
+        password_check = db.execute("SELECT * FROM users WHERE password = :password", {"password": password})  
 
         if not password_check:
             return render_template('index.html', message="Password incorrect.")  
 
-        user_id = db.execute("SELECT * FROM users WHERE username = :username AND password = :password" {"username": username, "password": password}).fetchone()    
+        user_id = db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).fetchone()    
         session["user_id"] = user_id.id
 
         return render_template('notes.html')
+
+    else:
+        return render_template('index.html')    
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -56,7 +60,7 @@ def register():
     if request.method == "GET":
         return render_template('register.html')
 
-    if method == "POST":
+    if request.method == "POST":
 
         # Get info from form
         username = request.form.get("user-name")
@@ -73,11 +77,31 @@ def register():
             return render_template('register.html', message="Username already exists")
 
         # Commit user data to db
-        db.execute("INSERT INTO users(username, password) VALUES (:username :password)", {"username": username, "password": password})    
+        db.execute("INSERT INTO users(username, password) VALUES (:username, :password)", {"username": username, "password": password})    
         db.commit()
 
         return render_template('index.html', message='Success! User has been created.')
 
-          
+@app.route('/notes', methods=["GET", "POST"])
+def notes():
+
+    # Get existing notes for user
+    notes = db.execute("SELECT * FROM notes WHERE userid = :userid", {"userid": session["user_id"]}).fetchall()
+    
+    if request.method == 'POST':
+
+        note = request.form.get('text')
+        timestamp = datetime.now()
+        userid = session['user_id']
+
+        db.execute('INSERT INTO notes(userid, text, timestamp) VALUES (:userid, :note, :timestamp)', {'userid': userid, 'note': note, 'timestamp': timestamp})
+        db.commit()
+
+        notes = db.execute("SELECT * FROM notes WHERE userid = :userid", {"userid": session["user_id"]}).fetchall()
+
+        return render_template('notes.html', notes=notes)
+
+    else:
+        return render_template('notes.html', notes=notes)    
 
 
